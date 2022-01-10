c572b8ef5b43b2cfb29f758540afe61f3a043637
Corbin-Arcus <87047392+Corbin-Arcus@users.noreply.github.com>
2022-01-10 16:08:29 -0500

    Server backend up and running (#7)
    * Get all servers Json working
    
    * server backend up and running

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+
 from .api.aws_image import image_routes
 from .api.server_routes import server_routes
 
