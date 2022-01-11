4726029417923e9076380a329a446fc9a9c93f6d
Seth <seth.holland@gmail.com>
2022-01-11 11:46:45 -0600

    added redirect if no servers loaded first

--- react-app/src/components/ChannelsPage/index.js
+++ react-app/src/components/ChannelsPage/index.js
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom'
 import { getAllChannels } from '../../store/channel';
+import { useHistory } from 'react-router';
 
 
 function ChannelsList() {
     const { serverId } = useParams();
     const dispatch = useDispatch();
+    let history = useHistory()
     const channels = useSelector(state => {
         return Object.values(state.channel);
+    })
+
+    const servers = useSelector(state => {
+        return Object.values(state.server);
     })
 
 
     useEffect(() => {
         dispatch(getAllChannels(serverId))
+
+        //if user has entered a url without clicking through the site, return them to the servers page
+        if (Object.keys(servers).length < 1) {
+            history.push(`/servers`)
+        }
+
     }, [dispatch])
 
 
