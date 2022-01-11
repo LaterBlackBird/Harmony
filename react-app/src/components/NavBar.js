d584fab51f4224a7e5366ac729adb88211eb8190
Seth Holland <seth.holland@gmail.com>
2022-01-11 16:13:07 -0600

    Create main page and start css (#18)
    * can edit channel name
    
    * delete channels operational, normalized data in channel state
    
    * code clean up
    
    * added redirect if no servers loaded first
    
    * starting to move routes into place
    
    * changed which links appear on navbar if user is logged in
    
    * basic grid layout with servers and channels in place

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,39 +1,36 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
+  let userLinks;
+  const user = useSelector(state => state.session.user);
+
+  if (!user) {
+    userLinks = (
+      <>
+        <NavLink to='/login' exact={true} activeClassName='active'>Login</NavLink>
+        <NavLink to='/sign-up' exact={true} activeClassName='active'>Sign Up</NavLink>
+      </>
+    )
+  } else {
+    userLinks = (
+      <>
+        <NavLink to='/users' exact={true} activeClassName='active'>Users</NavLink>
+        <LogoutButton />
+      </>
+    )
+  }
+
   return (
     <nav>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/channels/1/messages' exact={true} activeClassName='active'>
-            Messages
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
+          <NavLink to='/servers' exact={true} activeClassName='active'>Home</NavLink>
+          <NavLink to='/channels/1/messages' exact={true} activeClassName='active'>Messages</NavLink>
+          {userLinks}
         </li>
       </ul>
     </nav>
