b3f689c1dbf23e2a0e9dc6ef22baf1afb01fea30
Seth <seth.holland@gmail.com>
2022-01-14 08:56:01 -0600

    added tooltip to server hover

--- react-app/src/components/ServersPage/index.js
+++ react-app/src/components/ServersPage/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as serverActions from '../../store/server'
 import { Link, useParams, NavLink } from 'react-router-dom'
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 function ServerPage() {
   const dispatch = useDispatch()
@@ -19,20 +21,24 @@ function ServerPage() {
   return (
     <div id='servers_container'>
       <div className="server_info_block" id='convo_block'>
-        <NavLink to={`/servers/0/conversations/${user.id}`} exact={true} activeClassName='active' className='server_image server_icon'>
-          <i className="far fa-comments"></i>
-        </NavLink>
+        <Tippy content='Direct Messages' placement='right' theme='black'>
+          <NavLink to={`/servers/0/conversations/${user.id}`} exact={true} activeClassName='active' className='server_image server_icon'>
+            <i className="far fa-comments"></i>
+          </NavLink>
+        </Tippy>
         <div id="guild_seperator"></div>
       </div>
       {serversArr?.map(server =>
         <div key={server.id} className="server_info_block">
-          <Link to={`/servers/${server.id}/channels`} className='server_link'><img className={`server_image ${server.id === parseInt(serverId) ? 'selected' : ''}`} src={server.server_image} alt={server.server_name} /></Link>
-          <p className='hide'>{`${server.server_name}`}</p>
+          <Tippy content={server.server_name} placement='right' theme='black'>
+            <Link to={`/servers/${server.id}/channels`} className='server_link'><img className={`server_image ${server.id === parseInt(serverId) ? 'selected' : ''}`} src={server.server_image} alt={server.server_name} /></Link>
+          </Tippy>
         </div>
       )}
       <div className='server_info_block'>
-        <Link to={`/servers/new`} className='server_image server_icon'><i className="fas fa-plus"></i></Link>
-        <p className='hide'>Add A Server</p>
+        <Tippy content='Create A Server' placement='right' theme='black'>
+          <Link to={`/servers/new`} className='server_image server_icon'><i className="fas fa-plus"></i></Link>
+        </Tippy>
       </div>
     </div>
   )
