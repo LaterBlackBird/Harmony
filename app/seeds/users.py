96bb90c397a9f14e3f3e42a281e1d0476316c336
Seth <seth.holland@gmail.com>
2022-01-14 21:03:23 -0600

    updated user seeder file to have images

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', profile_image='https://humbleimages.s3.amazonaws.com/7d2414de391e414391c6242c1b4b2c7f.jpg')
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', profile_image='https://humbleimages.s3.amazonaws.com/ccce5ff01d58411c82baefdd069397f7.jpg')
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', profile_image='https://humbleimages.s3.amazonaws.com/ecb82a5a65ee4f8b9bb31e3853246ff1.jpg')
 
     db.session.add(demo)
     db.session.add(marnie)
